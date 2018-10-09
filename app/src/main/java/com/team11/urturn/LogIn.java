bfbb2ec6253c4da14fd775a35ece206bd37f3cbb
Nicholas Bucciarelli <bucciarellin16@students.ecu.edu>
2018-10-09 12:52:02 -0400

    Initial commit

--- app/src/main/java/com/team11/urturn/LogIn.java
+++ app/src/main/java/com/team11/urturn/LogIn.java
@@ -1,13 +1,51 @@
 package com.team11.urturn;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LogIn extends AppCompatActivity {
+
+    private EditText UserName;
+    private EditText Password;
+    private TextView Attempts;
+    private Button LoginB;
+    private int counter = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        UserName = (EditText) findViewById(R.id.Email);
+        Password = (EditText) findViewById(R.id.Password);
+        Attempts = (TextView) findViewById(R.id.Attempts);
+        LoginB = (Button) findViewById(R.id.LogInButton);
+
+        LoginB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(UserName.getText().toString(), Password.getText().toString());
+            }
+        });
+
     }
-}
+
+    private void validate(String userName, String userPassword){
+        if ((userName.equals("Admin") && (userPassword.equals("12345")))) {
+            Intent intent = new Intent(LogIn.this, MainActivity.class);
+            startActivity(intent);
+        }else{
+            counter --;
+            //Attempts.setText("Number of attempts remaining: " + String.valueOf(counter));
+
+            if (counter ==0);{
+                LoginB.setEnabled(false);
+            }
+        }
+        }
+    }
