1249188d1791d69bdd2430ea3aece4ef72ce8638
Nick <bucciarellin16@students.ecu.edu>
2018-10-10 21:12:07 -0400

    First Commit

--- app/src/main/java/com/team11/urturn/LogIn.java
+++ app/src/main/java/com/team11/urturn/LogIn.java
@@ -10,12 +10,19 @@ import android.widget.TextView;
 
 public class LogIn extends AppCompatActivity {
 
+    /**
+     * Variables, Constants, Buttons, Text
+     */
     private EditText UserName;
     private EditText Password;
-    private TextView Attempts;
+    //private TextView Attempts;
     private Button LoginB;
     private int counter = 5;
 
+    /**
+     * This method is called whenever the app is started
+     * @param savedInstanceState This is the starting State when the app is launched
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +30,7 @@ public class LogIn extends AppCompatActivity {
 
         UserName = (EditText) findViewById(R.id.Email);
         Password = (EditText) findViewById(R.id.Password);
-        Attempts = (TextView) findViewById(R.id.Attempts);
+        //Attempts = (TextView) findViewById(R.id.Attempts);
         LoginB = (Button) findViewById(R.id.LogInButton);
 
         LoginB.setOnClickListener(new View.OnClickListener() {
@@ -35,13 +42,20 @@ public class LogIn extends AppCompatActivity {
 
     }
 
+    /**
+     * This method is used to Validate the users credentials
+     * @param userName The user must enter a String userName to be validated
+     * @param userPassword The user must enter a String password to be validated
+     */
     private void validate(String userName, String userPassword){
         if ((userName.equals("Admin") && (userPassword.equals("12345")))) {
             Intent intent = new Intent(LogIn.this, MainActivity.class);
             startActivity(intent);
         }else{
+
             counter --;
-            //Attempts.setText("Number of attempts remaining: " + String.valueOf(counter));
+
+           // Attempts.setText(String.valueOf(counter));
 
             if (counter ==0);{
                 LoginB.setEnabled(false);
